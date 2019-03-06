721e052c1b955b609936a5f334082fdb1a864bb9
adel <upgamestudio@gmail.com>
2019-03-06 17:12:01 +0300

    Добавил игрокаи его перемещение

---
+++ LaserDefender/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal");
+        var newXpos = transform.position.x + deltaX;
+
+        transform.position = new Vector2(newXpos, transform.position.y);
+    }
+}
