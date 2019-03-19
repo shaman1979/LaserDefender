404532f16352d3997141b70c35ad857f4a38db11
adel <upgamestudio@gmail.com>
2019-03-19 11:32:22 +0300

    Добавил ограничения

--- LaserDefender/Assets/Scripts/Player.cs
+++ LaserDefender/Assets/Scripts/Player.cs
@@ -5,23 +5,59 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 1f;
+
+    [SerializeField] GameObject laserPrefabs;
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
     void Start()
     {
-        
+        SetUpMoveBoundaries();
     }
+
+ 
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject laser =  Instantiate(
+                laserPrefabs, 
+                transform.position, 
+                Quaternion.identity) as GameObject;
+
+        }
     }
 
     private void Move()
     {
-        var deltaX = Input.GetAxis("Horizontal");
-        var newXpos = transform.position.x + deltaX;
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
 
-        transform.position = new Vector2(newXpos, transform.position.y);
+        var newXpos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYpos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+
+        transform.position = new Vector2(newXpos, newYpos);
     }
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(Vector3.zero).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(Vector3.right).x - padding;
+
+        yMin = gameCamera.ViewportToWorldPoint(Vector3.zero).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(Vector3.up).y - padding;
+    }
+
 }
